fdc60cecfc3ad6dc5f11b4b11974794c2601a409
signekreicere <56268082+signekreicere@users.noreply.github.com>
2022-03-17 01:04:30 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://faa8c2cc.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
